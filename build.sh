a44df1b74f52c15d26ac434aa74a8a41037502d4
Eddy Westbrook <westbrook@galois.com>
2018-03-13 11:47:35 -0700

    modified build.sh script to deal with how dumb and frustrating stack is

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# Stupid stack does not seem to respect us telling it below not to build the
+# grappa-build project, so we have to ensure that there is a dummy Main.hs file
+cp grappa-build/DummyBuildMain.hs grappa-build/Main.hs
+
 stack build grappa:grappa-c
