1528537835191310fd452dcd0cee06cf7089ce1f
Eddy Westbrook <westbrook@galois.com>
2019-07-30 12:10:24 -0700

    got lbfgs++ C++ file to be working, presumably...

--- cbits/optimize_lbfgs.cpp
+++ cbits/optimize_lbfgs.cpp
@@ -5,8 +5,13 @@
 using Eigen::VectorXd;
 using namespace LBFGSpp;
 
+typedef struct VectorRep {
+  double *vdata;
+  Eigen::Index vsize;
+} VectorRep;
+
 // A function that can be optimized
-typedef double (*optfun) (const VectorXd& x, VectorXd& grad);
+typedef double (*optfun) (const double *x, double *grad);
 
 // C++ wrapper around a C function pointer, for use with LBFGS++
 class WrappedFun {
@@ -15,23 +20,25 @@ private:
 public:
   WrappedFun (optfun f_in) { f = f_in; }
   double operator()(const VectorXd& x, VectorXd& grad) {
-    return f (x, grad);
+    return f (((VectorRep*)&x)->vdata, ((VectorRep*)&grad)->vdata);
   }
 };
 
 // External entrypoint to call LBFGS++
-extern "C" double optimize_lbfgs (optfun f, VectorXd& params) {
+extern "C" double optimize_lbfgs (optfun f, uint32_t size, double *init_xs) {
   // Set up parameters
   LBFGSParam<double> param;
   param.epsilon = 1e-6;
   param.max_iterations = 100;
 
   // Create solver and function objects, and return value slot
+  VectorRep init_rep = { init_xs, size };
+  VectorXd *vect = (VectorXd*)&init_rep;
   LBFGSSolver<double> solver(param);
   WrappedFun fun (f);
   double val;
 
   // Call solver
-  solver.minimize (fun, params, val);
+  solver.minimize (fun, *vect, val);
   return val;
 }
